2c8affdcce04a1df0a54a55d0269d358749e3f58
hmjfk <61924124+hmjfk@users.noreply.github.com>
2026-07-25 06:12:06 +0900

    fix: syntax

--- src/stdc/cheader/config.c
+++ src/stdc/cheader/config.c
@@ -2,9 +2,9 @@ typedef long c_long;
 typedef unsigned long c_ulong;
 
 #if defined(_POSIX_C_SOURCE)
-enum posix_c_source = _POSIX_C_SOURCE;
+enum {posix_c_source = _POSIX_C_SOURCE};
 #endif
 
 #if defined(_XOPEN_SOURCE)
-enum xopen_source = _XOPEN_SOURCE;
+enum {xopen_source = _XOPEN_SOURCE};
 #endif
