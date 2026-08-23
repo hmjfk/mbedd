6e2d9d1899c95f7afb2dfe95d6f9c02091c889eb
hmjfk <61924124+hmjfk@users.noreply.github.com>
2026-08-23 15:50:33 +0900

    add: POSIX D headers

---
+++ src/stdc/locale.d
@@ -0,0 +1,31 @@
+/*
+    mbedd - 組み込みシステム向けのD言語標準ライブラリとDruntime
+    Copyright (C) 2025 - Denkousi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    Under Section 7 of GPL version 3, you are granted additional
+    permissions described in the GCC Runtime Library Exception, version
+    3.1, as published by the Free Software Foundation.
+
+    You should have received a copy of the GNU General Public License and
+    a copy of the GCC Runtime Library Exception along with this program;
+    see the files LICENSE and LICENSE.RUNTIME respectively.  If not, see
+    <http://www.gnu.org/licenses/>.
+*/
+/**
+Copyright:  Copyright Denkousi 2025-
+License:    $(LINK2 http://www.gnu.org/licenses/gpl.html, GPL3.0+) with $(LINK2 https://www.gnu.org/licenses/gcc-exception.html, GCC RLE).
+Authors:    Denkousi
+Source:     
+*/
+module stdc.locale;
+public import stdc.cheader.locale;
