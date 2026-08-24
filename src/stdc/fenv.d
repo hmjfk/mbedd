4c2fb9f3f7026291aa0a6475434918af35b76ee4
hmjfk <61924124+hmjfk@users.noreply.github.com>
2026-08-24 16:41:43 +0900

    change: libc decl tuning

--- src/stdc/fenv.d
+++ src/stdc/fenv.d
@@ -21,8 +21,160 @@
     see the files LICENSE and LICENSE.RUNTIME respectively.  If not, see
     <http://www.gnu.org/licenses/>.
 */
+/**
+Copyright:  Copyright Denkousi 2025-
+License:    $(LINK2 http://www.gnu.org/licenses/gpl.html, GPL3.0+) with $(LINK2 https://www.gnu.org/licenses/gcc-exception.html, GCC RLE).
+Authors:    Denkousi
+Source:     
+*/
 module stdc.fenv;
 public import stdc.cheader.fenv;
 
+// work only
+version(none)
+{
+    import core.stdc.fenv;
+}
+
+extern(C):
+@system:
+@nogc:
+nothrow:
+
 ///
 enum __STDC_VERSION_FENV_H__ = 202311L;
+
+version(D_Ddoc)
+{
+    private
+    {
+        enum see_description;
+        enum unspecified;
+        enum implementation_defined; 
+    }
+
+    ///
+    alias fenv_t = unspecified;
+    ///
+    alias femode_t = unspecified;
+    ///
+    alias fexcept_t = unspecified;
+
+    enum
+    {
+        ///
+        FE_DIVBYZERO = see_description,
+        ///
+        FE_INEXACT = see_description,
+        ///
+        FE_INVALID = see_description,
+        ///
+        FE_OVERFLOW = see_description,
+        ///
+        FE_UNDERFLOW = see_description,
+
+        ///
+        FE_DFL_MODE = implementation_defined,
+        
+        ///
+        FE_ALL_EXCEPT = see_description,
+        
+        ///
+        FE_DOWNWARD = see_description,
+        ///
+        FE_TONEAREST = see_description,
+        ///
+        FE_TONEARESTFROMZERO = see_description,
+        ///
+        FE_TOWARDZERO = see_description,
+        ///
+        FE_UPWARD = see_description,
+
+        /// 
+        FE_DEC_DOWNWARD = see_description,
+        ///
+        FE_DEC_TONEAREST = see_description,
+        ///
+        FE_DEC_TONEARESTFROMZERO = see_description,
+        ///
+        FE_DEC_TOWARDZERO = see_description,
+        ///
+        FE_DEC_UPWARD = see_description,
+
+        ///
+        FE_DFL_ENV = implementation_defined,
+    }
+
+}
+else
+{
+    static if(is(typeof(fe_divbyzero)))
+    enum FE_DIVBYZERO = fe_divbyzero;
+    static if(is(typeof(fe_inexact)))
+    enum FE_INEXACT = fe_inexact;
+    static if(is(typeof(fe_invalid)))
+    enum FE_INVALID = fe_invalid;
+    static if(is(typeof(fe_overflow)))
+    enum FE_OVERFLOW = fe_overflow;
+    static if(is(typeof(fe_underflow)))
+    enum FE_UNDERFLOW = fe_underflow;
+
+    enum FE_DFL_MODE = fe_dfl_mode;
+
+    enum FE_ALL_EXCEPT = fe_all_except;
+
+    static if(is(typeof(fe_downward)))
+    enum FE_DOWNWARD = fe_downward;
+    static if(is(typeof(fe_tonearest)))
+    enum FE_TONEAREST = fe_tonearest;
+    static if(is(typeof(fe_tonearestfromzero)))
+    enum FE_TONEARESTFROMZERO = fe_tonearestfromzero;
+    static if(is(typeof(fe_towardzero)))
+    enum FE_TOWARDZERO = fe_towardzero;
+    static if(is(typeof(fe_upward)))
+    enum FE_UPWARD = fe_upward;
+
+    static if(is(typeof(fe_dec_downward)))
+    enum FE_DEC_DOWNWARD = fe_dec_downward;
+    static if(is(typeof(fe_dec_tonearest)))
+    enum FE_DEC_TONEAREST = fe_dec_tonearest;
+    static if(is(typeof(fe_dec_tonearestfromzero)))
+    enum FE_DEC_TONEARESTFROMZERO = fe_dec_tonearestfromzero;
+    static if(is(typeof(fe_dec_towardzero)))
+    enum FE_DEC_TOWARDZERO = fe_dec_towardzero;
+    static if(is(typeof(fe_dec_upward)))
+    enum FE_DEC_UPWARD = fe_dec_upward;
+
+    enum FE_DFL_ENV = fe_dfl_env();
+}
+
+///
+int feclearexcept(int excepts);
+///
+int fegetexceptflag(fexcept_t* flagp, int excepts);
+///
+int feraiseexcept(int excepts);
+///
+int fesetexcept(int excepts);
+///
+int fesetexceptflag(scope const(fexcept_t*) flagp, int excepts);
+///
+int fetestexceptflag(scope const(fexcept_t*) flagp, int excepts);
+///
+int fetestexcept(int excepts);
+///
+int fegetmode(femode_t* modep);
+///
+int fegetround();
+///
+int fesetmode(scope const(femode_t*) modep);
+///
+int fesetround(int rnd);
+///
+int fegetenv(fenv_t* envp);
+///
+int feholdexcept(fenv_t* envp);
+///
+int fesetenv(scope const(fenv_t*) envp);
+///
+int feupdateenv(scope const(fenv_t*) envp);
--- src/stdc/threads.d
+++ src/stdc/threads.d
@@ -23,3 +23,75 @@
 */
 module stdc.threads;
 public import stdc.cheader.threads;
+
+///
+enum ONCE_FLAG_INIT = once_flag.init;
+
+version(D_Ddoc)
+{
+    private
+    {
+        enum see_description = null;
+        alias see_below = void;
+    }
+    ///
+    enum TSS_DTOR_ITERATIONS = see_description;
+    ///
+    alias cnd_t = see_below;
+    ///
+    alias thrd_t = see_below;
+    ///
+    alias tss_t = see_below;
+    ///
+    alias mtx_t = see_below;
+}
+///
+void call_once(once_flag* flag, void function() func);
+///
+int cnd_broadcast(cnd_t* cond);
+///
+void cnd_destroy(cnd_t* cond);
+///
+int cnd_init(cnd_t* cond);
+///
+int cnd_signal(cnd_t* cond);
+///
+int cnd_timedwait(cnd_t* cond, mtx_t* mtx, const timespec* ts);
+///
+int cnd_wait(cnd_t* cond, mtx_t* mtx);
+///
+void mtx_destroy(mtx_t* mtx);
+///
+int mtx_init(mtx_t* mtx, int type);
+///
+int mtx_lock(mtx_t* mtx);
+///
+int mtx_timedlock(mtx_t* mtx, const timespec* ts);
+///
+int mtx_trylock(mtx_t* mtx);
+///
+int mtx_unlock(mtx_t* mtx);
+///
+int thrd_create(thrd_t* thr, thrd_start_t func, void* arg);
+///
+thrd_t thrd_current();
+///
+int thrd_detach(thrd_t thr);
+///
+int thrd_equal(thrd_t thr0, thrd_t thr1);
+///
+noreturn thrd_exit(int res);
+///
+int thrd_join(thrd_t thr, int* res);
+///
+int thrd_sleep(const timespec* duration, timespec* remaining);
+///
+void thrd_yield();
+///
+int tss_create(tss_t* key, tss_dtor_t dtor);
+///
+void tss_delete(tss_t key);
+///
+void* tss_get(tss_t key);
+///
+int tss_set(tss_t key, void* val);
