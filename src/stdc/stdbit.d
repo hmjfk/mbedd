d0970c62fe7e79398488aca0368bd2e08b76f962
hmjfk <61924124+hmjfk@users.noreply.github.com>
2026-09-02 14:33:48 +0900

    change: add stdc.stdbit D version decl

--- src/stdc/stdbit.d
+++ src/stdc/stdbit.d
@@ -22,8 +22,14 @@
     <http://www.gnu.org/licenses/>.
 */
 module stdc.stdbit;
-public import stdc.cheader.stdbit;
+public import stdc.stdint;
+public import stdc.config: c_ulong;
+public import stdc.limits;
 
+extern (C):
+nothrow:
+@nogc:
+@live:
 
 /// ditto
 enum : bool
@@ -48,6 +54,17 @@ else version(LittleEndian)
     enum __STDC_ENDIAN_NATIVE__ =  __STDC_ENDIAN_LITTLE__;
 }
 
+///
+uint stdc_leading_zeros_uc(ubyte value) pure;
+/// ditto
+uint stdc_leading_zeros_us(ushort value) pure;
+/// ditto
+uint stdc_leading_zeros_ui(uint value) pure;
+/// ditto
+uint stdc_leading_zeros_ul(c_ulong value) pure;
+/// ditto
+uint stdc_leading_zeros_ull(ulong value) pure;
+
 /// stdc_leading_zeros function generic macro
 alias stdc_leading_zeros = stdc_leading_zeros_uc;
 /// ditto
@@ -58,6 +75,18 @@ alias stdc_leading_zeros = stdc_leading_zeros_ui;
 alias stdc_leading_zeros = stdc_leading_zeros_ul;
 /// ditto
 alias stdc_leading_zeros = stdc_leading_zeros_ull;
+
+/// ditto
+uint stdc_leading_ones_uc(ubyte value) pure;
+/// ditto
+uint stdc_leading_ones_us(ushort value) pure;
+/// ditto
+uint stdc_leading_ones_ui(uint value) pure;
+/// ditto
+uint stdc_leading_ones_ul(c_ulong value) pure;
+/// ditto
+uint stdc_leading_ones_ull(ulong value) pure;
+/// ditto
 
 /// stdc_leading_ones function generic macro
 alias stdc_leading_ones = stdc_leading_ones_uc;
@@ -70,6 +99,16 @@ alias stdc_leading_ones = stdc_leading_ones_ul;
 /// ditto
 alias stdc_leading_ones = stdc_leading_ones_ull;
 
+/// ditto
+uint stdc_trailing_zeros_uc(ubyte value) pure;
+/// ditto
+uint stdc_trailing_zeros_us(ushort value) pure;
+/// ditto
+uint stdc_trailing_zeros_ui(uint value) pure;
+/// ditto
+uint stdc_trailing_zeros_ul(c_ulong value) pure;
+/// ditto
+uint stdc_trailing_zeros_ull(ulong value) pure;
 
 /// stdc_trailing_zeros function generic macro
 alias stdc_trailing_zeros = stdc_leading_ones_uc;
@@ -82,6 +121,17 @@ alias stdc_trailing_zeros = stdc_leading_ones_ul;
 /// ditto
 alias stdc_trailing_zeros = stdc_leading_ones_ull;
 
+/// ditto
+uint stdc_trailing_ones_uc(ubyte value) pure;
+/// ditto
+uint stdc_trailing_ones_us(ushort value) pure;
+/// ditto
+uint stdc_trailing_ones_ui(uint value) pure;
+/// ditto
+uint stdc_trailing_ones_ul(c_ulong value) pure;
+/// ditto
+uint stdc_trailing_ones_ull(ulong value) pure;
+
 /// stdc_trailing_ones function generic macro
 alias stdc_trailing_ones = stdc_trailing_ones_uc;
 /// ditto
@@ -92,6 +142,17 @@ alias stdc_trailing_ones = stdc_trailing_ones_ui;
 alias stdc_trailing_ones = stdc_trailing_ones_ul;
 /// ditto
 alias stdc_trailing_ones = stdc_trailing_ones_ull;
+
+/// ditto
+uint stdc_first_leading_zero_uc(ubyte value) pure;
+/// ditto
+uint stdc_first_leading_zero_us(ushort value) pure;
+/// ditto
+uint stdc_first_leading_zero_ui(uint value) pure;
+/// ditto
+uint stdc_first_leading_zero_ul(c_ulong value) pure;
+/// ditto
+uint stdc_first_leading_zero_ull(ulong value) pure;
 
 /// stdc_first_leading_zero function generic macro
 alias stdc_first_leading_zero = stdc_first_leading_zero_uc;
@@ -104,6 +165,17 @@ alias stdc_first_leading_zero = stdc_first_leading_zero_ul;
 /// ditto
 alias stdc_first_leading_zero = stdc_first_leading_zero_ull;
 
+/// ditto
+uint stdc_first_leading_one_uc(ubyte value) pure;
+/// ditto
+uint stdc_first_leading_one_us(ushort value) pure;
+/// ditto
+uint stdc_first_leading_one_ui(uint value) pure;
+/// ditto
+uint stdc_first_leading_one_ul(c_ulong value) pure;
+/// ditto
+uint stdc_first_leading_one_ull(ulong value) pure;
+
 /// stdc_first_leading_one function generic macro
 alias stdc_first_leading_one = stdc_first_leading_one_uc;
 /// ditto
@@ -114,6 +186,17 @@ alias stdc_first_leading_one = stdc_first_leading_one_ui;
 alias stdc_first_leading_one = stdc_first_leading_one_ul;
 /// ditto
 alias stdc_first_leading_one = stdc_first_leading_one_ull;
+
+/// ditto
+uint stdc_first_trailing_zero_uc(ubyte value) pure;
+/// ditto
+uint stdc_first_trailing_zero_us(ushort value) pure;
+/// ditto
+uint stdc_first_trailing_zero_ui(uint value) pure;
+/// ditto
+uint stdc_first_trailing_zero_ul(c_ulong value) pure;
+/// ditto
+uint stdc_first_trailing_zero_ull(ulong value) pure;
 
 /// stdc_first_trailing_zero function generic macro
 alias stdc_first_trailing_zero = stdc_first_trailing_zero_uc;
@@ -126,6 +209,17 @@ alias stdc_first_trailing_zero = stdc_first_trailing_zero_ul;
 /// ditto
 alias stdc_first_trailing_zero = stdc_first_trailing_zero_ull;
 
+/// ditto
+uint stdc_first_trailing_one_uc(ubyte value) pure;
+/// ditto
+uint stdc_first_trailing_one_us(ushort value) pure;
+/// ditto
+uint stdc_first_trailing_one_ui(uint value) pure;
+/// ditto
+uint stdc_first_trailing_one_ul(c_ulong value) pure;
+/// ditto
+uint stdc_first_trailing_one_ull(ulong value) pure;
+
 /// stdc_first_trailing_one function generic macro
 alias stdc_first_trailing_one = stdc_first_trailing_one_uc;
 /// ditto
@@ -136,6 +230,17 @@ alias stdc_first_trailing_one = stdc_first_trailing_one_ui;
 alias stdc_first_trailing_one = stdc_first_trailing_one_ul;
 /// ditto
 alias stdc_first_trailing_one = stdc_first_trailing_one_ull;
+
+/// ditto
+uint stdc_count_zeros_uc(ubyte value) pure;
+/// ditto
+uint stdc_count_zeros_us(ushort value) pure;
+/// ditto
+uint stdc_count_zeros_ui(uint value) pure;
+/// ditto
+uint stdc_count_zeros_ul(c_ulong value) pure;
+/// ditto
+uint stdc_count_zeros_ull(ulong value) pure;
 
 /// stdc_count_zeros function generic macro
 alias stdc_count_zeros = stdc_count_zeros_uc;
@@ -148,6 +253,17 @@ alias stdc_count_zeros = stdc_count_zeros_ul;
 /// ditto
 alias stdc_count_zeros = stdc_count_zeros_ull;
 
+/// ditto
+uint stdc_count_ones_uc(ubyte value) pure;
+/// ditto
+uint stdc_count_ones_us(ushort value) pure;
+/// ditto
+uint stdc_count_ones_ui(uint value) pure;
+/// ditto
+uint stdc_count_ones_ul(c_ulong value) pure;
+/// ditto
+uint stdc_count_ones_ull(ulong value) pure;
+
 /// stdc_count_ones function generic macro
 alias stdc_count_ones = stdc_count_ones_uc;
 /// ditto
@@ -158,6 +274,17 @@ alias stdc_count_ones = stdc_count_ones_ui;
 alias stdc_count_ones = stdc_count_ones_ul;
 /// ditto
 alias stdc_count_ones = stdc_count_ones_ull;
+
+/// ditto
+bool stdc_has_single_bit_uc(ubyte value) pure;
+/// ditto
+bool stdc_has_single_bit_us(ushort value) pure;
+/// ditto
+bool stdc_has_single_bit_ui(uint value) pure;
+/// ditto
+bool stdc_has_single_bit_ul(c_ulong value) pure;
+/// ditto
+bool stdc_has_single_bit_ull(ulong value) pure;
 
 /// stdc_has_single_bit function generic macro
 alias stdc_has_single_bit = stdc_has_single_bit_uc;
@@ -170,6 +297,17 @@ alias stdc_has_single_bit = stdc_has_single_bit_ul;
 /// ditto
 alias stdc_has_single_bit = stdc_has_single_bit_ull;
 
+/// ditto
+uint stdc_bit_width_uc(ubyte value) pure;
+/// ditto
+uint stdc_bit_width_us(ushort value) pure;
+/// ditto
+uint stdc_bit_width_ui(uint value) pure;
+/// ditto
+uint stdc_bit_width_ul(c_ulong value) pure;
+/// ditto
+uint stdc_bit_width_ull(ulong value) pure;
+
 /// stdc_bit_width function generic macro
 alias stdc_bit_width = stdc_bit_width_uc;
 /// ditto
@@ -180,6 +318,17 @@ alias stdc_bit_width = stdc_bit_width_ui;
 alias stdc_bit_width = stdc_bit_width_ul;
 /// ditto
 alias stdc_bit_width = stdc_bit_width_ull;
+
+/// ditto
+ubyte stdc_bit_floor_uc(ubyte value) pure;
+/// ditto
+ushort stdc_bit_floor_us(ushort value) pure;
+/// ditto
+uint stdc_bit_floor_ui(uint value) pure;
+/// ditto
+c_ulong stdc_bit_floor_ul(c_ulong value) pure;
+/// ditto
+ulong stdc_bit_floor_ull(ulong value) pure;
 
 /// stdc_bit_floor function generic macro
 alias stdc_bit_floor = stdc_bit_floor_uc;
@@ -192,6 +341,18 @@ alias stdc_bit_floor = stdc_bit_floor_ul;
 /// ditto
 alias stdc_bit_floor = stdc_bit_floor_ull;
 
+/// ditto
+ubyte stdc_bit_ceil_uc(ubyte value) pure;
+/// ditto
+ushort stdc_bit_ceil_us(ushort value) pure;
+/// ditto
+uint stdc_bit_ceil_ui(uint value) pure;
+/// ditto
+c_ulong stdc_bit_ceil_ul(c_ulong value) pure;
+/// ditto
+ulong stdc_bit_ceil_ull(ulong value) pure;
+
+
 /// stdc_bit_ceil function generic macro
 alias stdc_bit_ceil = stdc_bit_ceil_uc;
 /// ditto
@@ -202,6 +363,18 @@ alias stdc_bit_ceil = stdc_bit_ceil_ui;
 alias stdc_bit_ceil = stdc_bit_ceil_ul;
 /// ditto
 alias stdc_bit_ceil = stdc_bit_ceil_ull;
+
+/// ditto
+ubyte stdc_rotate_left_uc(ubyte value, uint count) pure;
+/// ditto
+ushort stdc_rotate_left_us(ushort value, uint count) pure;
+/// ditto
+uint stdc_rotate_left_ui(uint value, uint count) pure;
+/// ditto
+c_ulong stdc_rotate_left_ul(c_ulong value, uint count) pure;
+/// ditto
+ulong stdc_rotate_left_ull(ulong value, uint count) pure;
+
 
 /// stdc_rotate_left function generic macro
 alias stdc_rotate_left = stdc_rotate_left_uc;
@@ -214,6 +387,18 @@ alias stdc_rotate_left = stdc_rotate_left_ul;
 /// ditto
 alias stdc_rotate_left = stdc_rotate_left_ull;
 
+/// ditto
+ubyte stdc_rotate_right_uc(ubyte value, uint count) pure;
+/// ditto
+ushort stdc_rotate_right_us(ushort value, uint count) pure;
+/// ditto
+uint stdc_rotate_right_ui(uint value, uint count) pure;
+/// ditto
+c_ulong stdc_rotate_right_ul(c_ulong value, uint count) pure;
+/// ditto
+ulong stdc_rotate_right_ull(ulong value, uint count) pure;
+
+
 /// stdc_rotate_right function generic macro
 alias stdc_rotate_right = stdc_rotate_right_uc;
 /// ditto
@@ -224,3 +409,164 @@ alias stdc_rotate_right = stdc_rotate_right_ui;
 alias stdc_rotate_right = stdc_rotate_right_ul;
 /// ditto
 alias stdc_rotate_right = stdc_rotate_right_ull;
+
+static if(is(typeof(CHAR_BIT))) // importCの対応C規格が古いため、異常を診断するかもしれない。
+static if (CHAR_BIT == 8)
+{
+    ///
+    void stdc_memreverse8(size_t n, ubyte* ptr);
+    /// ditto
+    uint8_t stdc_memreverse8u8(uint8_t value);
+    /// ditto
+    uint16_t stdc_memreverse8u16(uint16_t value);
+    /// ditto
+    uint32_t stdc_memreverse8u32(uint32_t value);
+    /// ditto
+    uint64_t stdc_memreverse8u64(uint64_t value);
+
+    /// 8bit load functions
+    /// ditto
+    uint_least8_t stdc_load8_leu8(const(ubyte)* ptr);
+    /// ditto
+    uint_least16_t stdc_load8_leu16(const(ubyte)* ptr);
+    /// ditto
+    uint_least32_t stdc_load8_leu32(const(ubyte)* ptr);
+    /// ditto
+    uint_least64_t stdc_load8_leu64(const(ubyte)* ptr);
+
+    /// ditto
+    uint_least8_t stdc_load8_beu8(const(ubyte)* ptr);
+    /// ditto
+    uint_least16_t stdc_load8_beu16(const(ubyte)* ptr);
+    /// ditto
+    uint_least32_t stdc_load8_beu32(const(ubyte)* ptr);
+    /// ditto
+    uint_least64_t stdc_load8_beu64(const(ubyte)* ptr);
+
+    /// ditto
+    uint_least8_t stdc_load8_aligned_leu8(const(ubyte)* ptr);
+    /// ditto
+    uint_least16_t stdc_load8_aligned_leu16(const(ubyte)* ptr);
+    /// ditto
+    uint_least32_t stdc_load8_aligned_leu32(const(ubyte)* ptr);
+    /// ditto
+    uint_least64_t stdc_load8_aligned_leu64(const(ubyte)* ptr);
+
+    /// ditto
+    uint_least8_t stdc_load8_aligned_beu8(const(ubyte)* ptr);
+    /// ditto
+    uint_least16_t stdc_load8_aligned_beu16(const(ubyte)* ptr);
+    /// ditto
+    uint_least32_t stdc_load8_aligned_beu32(const(ubyte)* ptr);
+    /// ditto
+    uint_least64_t stdc_load8_aligned_beu64(const(ubyte)* ptr);
+
+    /// ditto
+    int_least8_t stdc_load8_les8(const(ubyte)* ptr);
+    /// ditto
+    int_least16_t stdc_load8_les16(const(ubyte)* ptr);
+    /// ditto
+    int_least32_t stdc_load8_les32(const(ubyte)* ptr);
+    /// ditto
+    int_least64_t stdc_load8_les64(const(ubyte)* ptr);
+
+    /// ditto
+    int_least8_t stdc_load8_bes8(const(ubyte)* ptr);
+    /// ditto
+    int_least16_t stdc_load8_bes16(const(ubyte)* ptr);
+    /// ditto
+    int_least32_t stdc_load8_bes32(const(ubyte)* ptr);
+    /// ditto
+    int_least64_t stdc_load8_bes64(const(ubyte)* ptr);
+
+    /// ditto
+    int_least8_t stdc_load8_aligned_les8(const(ubyte)* ptr);
+    /// ditto
+    int_least16_t stdc_load8_aligned_les16(const(ubyte)* ptr);
+    /// ditto
+    int_least32_t stdc_load8_aligned_les32(const(ubyte)* ptr);
+    /// ditto
+    int_least64_t stdc_load8_aligned_les64(const(ubyte)* ptr);
+
+    /// ditto
+    int_least8_t stdc_load8_aligned_bes8(const(ubyte)* ptr);
+    /// ditto
+    int_least16_t stdc_load8_aligned_bes16(const(ubyte)* ptr);
+    /// ditto
+    int_least32_t stdc_load8_aligned_bes32(const(ubyte)* ptr);
+    /// ditto
+    int_least64_t stdc_load8_aligned_bes64(const(ubyte)* ptr);
+
+
+    /// 8bit store functions
+    uint_least8_t stdc_store8_leu8(const(ubyte)* ptr);
+    /// ditto
+    uint_least16_t stdc_store8_leu16(const(ubyte)* ptr);
+    /// ditto
+    uint_least32_t stdc_store8_leu32(const(ubyte)* ptr);
+    /// ditto
+    uint_least64_t stdc_store8_leu64(const(ubyte)* ptr);
+
+    /// ditto
+    uint_least8_t stdc_store8_beu8(const(ubyte)* ptr);
+    /// ditto
+    uint_least16_t stdc_store8_beu16(const(ubyte)* ptr);
+    /// ditto
+    uint_least32_t stdc_store8_beu32(const(ubyte)* ptr);
+    /// ditto
+    uint_least64_t stdc_store8_beu64(const(ubyte)* ptr);
+
+    /// ditto
+    uint_least8_t stdc_store8_aligned_leu8(const(ubyte)* ptr);
+    /// ditto
+    uint_least16_t stdc_store8_aligned_leu16(const(ubyte)* ptr);
+    /// ditto
+    uint_least32_t stdc_store8_aligned_leu32(const(ubyte)* ptr);
+    /// ditto
+    uint_least64_t stdc_store8_aligned_leu64(const(ubyte)* ptr);
+
+    /// ditto
+    uint_least8_t stdc_store8_aligned_beu8(const(ubyte)* ptr);
+    /// ditto
+    uint_least16_t stdc_store8_aligned_beu16(const(ubyte)* ptr);
+    /// ditto
+    uint_least32_t stdc_store8_aligned_beu32(const(ubyte)* ptr);
+    /// ditto
+    uint_least64_t stdc_store8_aligned_beu64(const(ubyte)* ptr);
+
+    /// ditto
+    int_least8_t stdc_store8_les8(const(ubyte)* ptr);
+    /// ditto
+    int_least16_t stdc_store8_les16(const(ubyte)* ptr);
+    /// ditto
+    int_least32_t stdc_store8_les32(const(ubyte)* ptr);
+    /// ditto
+    int_least64_t stdc_store8_les64(const(ubyte)* ptr);
+
+    /// ditto
+    int_least8_t stdc_store8_bes8(const(ubyte)* ptr);
+    /// ditto
+    int_least16_t stdc_store8_bes16(const(ubyte)* ptr);
+    /// ditto
+    int_least32_t stdc_store8_bes32(const(ubyte)* ptr);
+    /// ditto
+    int_least64_t stdc_store8_bes64(const(ubyte)* ptr);
+
+    /// ditto
+    int_least8_t stdc_store8_aligned_les8(const(ubyte)* ptr);
+    /// ditto
+    int_least16_t stdc_store8_aligned_les16(const(ubyte)* ptr);
+    /// ditto
+    int_least32_t stdc_store8_aligned_les32(const(ubyte)* ptr);
+    /// ditto
+    int_least64_t stdc_store8_aligned_les64(const(ubyte)* ptr);
+
+    /// ditto
+    int_least8_t stdc_store8_aligned_bes8(const(ubyte)* ptr);
+    /// ditto
+    int_least16_t stdc_store8_aligned_bes16(const(ubyte)* ptr);
+    /// ditto
+    int_least32_t stdc_store8_aligned_bes32(const(ubyte)* ptr);
+    /// ditto
+    int_least64_t stdc_store8_aligned_bes64(const(ubyte)* ptr);
+}
