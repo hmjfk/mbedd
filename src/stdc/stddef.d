9b23f76ea7207acb4c623cc9c204bda776d1ed97
hmjfk <61924124+hmjfk@users.noreply.github.com>
2026-09-07 12:01:54 +0900

    docs: stdc.stddef

--- src/stdc/stddef.d
+++ src/stdc/stddef.d
@@ -34,6 +34,15 @@ import stdc.config: nullptr;
 ///
 enum NULL = nullptr;
 
+version(D_Ddoc)
+{
+    private enum see_description;
+    
+    alias size_t = typeof(0.sizeof);
+    alias ptrdiff_t = typeof(typeof(cast(void*)0 - cast(void*)0));
+    alias wchar_t = see_description;
+}
+
 static if(!is(rsize_t))
 version(__STDC_WANT_LIB_EXT1__)
 {
