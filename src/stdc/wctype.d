048b051dc5a7c1200371506437a81eebd7f888e0
hmjfk <61924124+hmjfk@users.noreply.github.com>
2026-09-08 12:21:18 +0900

    add: stdc.wctype decl

--- src/stdc/wctype.d
+++ src/stdc/wctype.d
@@ -22,4 +22,90 @@
     <http://www.gnu.org/licenses/>.
 */
 module stdc.wctype;
-public import stdc.cheader.wctype;
+public import stdc.cheader.wctype: wctrans_t, wctype_t;
+public import stdc.locale: locale_t;
+public import stdc.wchar_: wchar_t;
+
+extern (C):
+@trusted:
+nothrow:
+@nogc:
+
+version(none)
+import core.stdc.wctype;
+
+pure int iswalnum(wint_t);
+///
+int iswalnum_l(wint_t, locale_t);
+///
+pure int iswalpha(wint_t);
+///
+int iswalpha_l(wint_t, locale_t);
+///
+pure int iswblank(wint_t);
+///
+int iswblank_l(wint_t, locale_t);
+///
+pure int iswcntrl(wint_t);
+///
+int iswcntrl_l(wint_t, locale_t);
+///
+pure int iswctype(wint_t, wctype_t);
+///
+int iswctype_l(wint_t, wctype_t, locale_t);
+///
+pure int iswdigit(wint_t);
+///
+int iswdigit_l(wint_t, locale_t);
+///
+pure int iswgraph(wint_t);
+///
+int iswgraph_l(wint_t, locale_t);
+///
+pure int iswlower(wint_t);
+///
+int iswlower_l(wint_t, locale_t);
+///
+pure int iswprint(wint_t);
+///
+int iswprint_l(wint_t, locale_t);
+///
+pure int iswpunct(wint_t);
+///
+int iswpunct_l(wint_t, locale_t);
+///
+pure int iswspace(wint_t);
+///
+int iswspace_l(wint_t, locale_t);
+///
+pure int iswupper(wint_t);
+///
+int iswupper_l(wint_t, locale_t);
+///
+pure int iswxdigit(wint_t);
+///
+int iswxdigit_l(wint_t, locale_t);
+///
+wint_t towctrans(wint_t, wctrans_t);
+///
+wint_t towctrans_l(wint_t, wctrans_t, locale_t);
+///
+pure wint_t towlower(wint_t);
+///
+wint_t towlower_l(wint_t, locale_t);
+///
+pure wint_t towupper(wint_t);
+///
+wint_t towupper_l(wint_t, locale_t);
+
+@system
+{
+    ///
+    wctrans_t wctrans(const(char*));
+    ///
+    wctrans_t wctrans_l(const(char*), locale_t);
+    ///
+    wctype_t  wctype(const(char*));
+    ///
+    wctype_t  wctype_l(const(char*), locale_t);
+}
