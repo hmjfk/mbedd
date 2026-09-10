d3849ee14c4874e2797d29675364db2bf3508bca
hmjfk <61924124+hmjfk@users.noreply.github.com>
2026-09-10 21:11:45 +0900

    docs: comment location

--- src/stdcpp/compare.d
+++ src/stdcpp/compare.d
@@ -202,7 +202,6 @@ extern(C++, "std")
         ///
         alias is_transparent = unspecified;
     }
-    /// [cmp.alg], comparison algorithms
     version(D_Ddoc)
     {
         mixin template strong_order(){}
@@ -212,7 +211,8 @@ extern(C++, "std")
         mixin template compare_weak_order_fallback(){}
         mixin template compare_partial_order_fallback(){}
     }
-    ///
+
+    /// [cmp.alg], comparison algorithms
     mixin strong_order;
     ///
     mixin weak_order;
@@ -224,7 +224,6 @@ extern(C++, "std")
     mixin compare_weak_order_fallback;
     ///
     mixin compare_partial_order_fallback;
-
 
     /// [compare.type], type ordering
     struct type_order(T, U)
